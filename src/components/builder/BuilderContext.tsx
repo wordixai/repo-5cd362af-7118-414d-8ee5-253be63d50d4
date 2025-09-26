48b5d02a0fe30faefc342721e7b4cfacfd69a8bd
wordixai <wordixai@gmail.com>
2025-09-26 18:48:27 +0800

    Update files

--- src/components/builder/BuilderContext.tsx
+++ src/components/builder/BuilderContext.tsx
@@ -215,7 +215,7 @@ export function BuilderProvider({ children }: { children: ReactNode }) {
 export function useBuilder() {
   const context = useContext(BuilderContext);
   if (!context) {
-    throw new Error('useBuilder must be used within a BuilderProvi');
+    throw new Error('useBuilder must be used within a');
   }
   return context;
 }
